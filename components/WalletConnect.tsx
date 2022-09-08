1985eb133a83466467893aa3c29ad09637068994
rodrigomd94 <51804048+rodrigomd94@users.noreply.github.com>
2022-09-07 20:18:38 -0600

    Initial commit

---
+++ components/WalletConnect.tsx
@@ -0,0 +1,73 @@
+import { Lucid, Blockfrost, utf8ToHex, C } from "lucid-cardano";
+import { useState, useEffect } from 'react';
+import { useStoreActions, useStoreState } from "../utils/store";
+
+
+const WalletConnect = () => {
+    // const [availableWallets, setAvailableWallets] = useState<string[]>([])
+    const walletStore = useStoreState(state => state.wallet)
+    const setWallet = useStoreActions(actions => actions.setWallet)
+    const availableWallets = useStoreState(state => state.availableWallets)
+    const setAvailableWallets = useStoreActions(actions => actions.setAvailableWallets)
+
+    const [connectedAddress, setConnectedAddress] = useState("")
+
+    const initLucid = async (wallet: string) => {
+        const api = (await window.cardano[
+            wallet.toLowerCase()
+        ].enable())
+
+        const lucid = await Lucid.new(
+            new Blockfrost('https://cardano-mainnet.blockfrost.io/api/v0', process.env.NEXT_PUBLIC_BLOCKFROST as string),
+            'Mainnet')
+        lucid.selectWallet(api)
+        return lucid;
+    }
+
+    const walletConnected = async (wallet: string, connect: boolean = true) => {
+        const addr = connect ? await (await initLucid(wallet)).wallet.address() : ''
+        const walletStoreObj = connect ? { connected: true, name: wallet, address: addr } : { connected: false, name: '', address: '' }
+        setConnectedAddress(addr)
+        setWallet(walletStoreObj)
+    }
+
+    const selectWallet = async (wallet: string) => {
+        if (
+            window.cardano &&
+            (await window.cardano[wallet.toLocaleLowerCase()].enable())
+        ) {
+            walletConnected(wallet)
+        }
+    }
+
+    useEffect(() => {
+        console.log(walletStore)
+
+        //  setConnectedAddress(walletStore.address)
+    }, [walletStore])
+
+    useEffect(() => {
+        let wallets = []
+        if (window.cardano) {
+            if (window.cardano.nami) wallets.push('Nami')
+            if (window.cardano.eternl) wallets.push('Eternl')
+            if (window.cardano.flint) wallets.push('Flint')
+        }
+        setAvailableWallets(wallets)
+    }, [])
+
+    return (
+        <>
+            <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn m-1">{connectedAddress != "" ? 'Connected' : 'Connect'}</label>
+                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                    {availableWallets.map((wallet) =>
+                        <li key={wallet} onClick={() => { selectWallet(wallet) }} ><a>{wallet}</a></li>
+                    )}
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default WalletConnect;
